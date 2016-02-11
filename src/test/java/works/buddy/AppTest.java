ad4e3531442df2c215d60ee4ad2496a6e86d91b3
Marcin Kudla <marcin.kudla@gmail.com>
2016-02-11 10:40:34 +0100

    added test

---
+++ src/test/java/works/buddy/AppTest.java
@@ -0,0 +1,13 @@
+package works.buddy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testApp() throws InterruptedException {
+        Thread.sleep(5000);
+        Assert.assertTrue(true);
+    }
+}
