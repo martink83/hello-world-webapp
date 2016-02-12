8b87841daf246dec88d9e111fdb7785cd9431a08
martink83 <marcin.kudla@gmail.com>
2016-02-12 09:58:18 +0100

    Update AppTest.java

--- src/test/java/works/buddy/AppTest.java
+++ src/test/java/works/buddy/AppTest.java
@@ -7,7 +7,7 @@ public class AppTest {
 
     @Test
     public void testApp() throws InterruptedException {
-        Thread.sleep(5000);
+        Thread.sleep(5000); 
         Assert.assertTrue(true);
     }
 }
